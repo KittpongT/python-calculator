94b6d4b4fe9f6b662a9aae7aeb3f9d0af5bf4071
Inteoty <57790837+KittpongT@users.noreply.github.com>
2024-11-18 14:16:27 +0000

    Fixed bugs

--- calculator.py
+++ calculator.py
@@ -3,25 +3,51 @@ class Calculator:
         return a + b
 
     def subtract(self, a, b):
-        return b - a
+        return a - b
 
     def multiply(self, a, b):
         result = 0
-        for i in range(b+1):
-            result = self.add(result, a)
+        if b >= 0:
+            for i in range(b):
+                result = self.add(result, a)
+        else:
+            for i in range(-b):
+                result = self.subtract(result, a)
         return result
 
     def divide(self, a, b):
         result = 0
-        while a > b:
-            a = self.subtract(a, b)
-            result += 1
+        negA = False
+        negB = False
+        if a < 0 : 
+            negA = True
+            a = self.multiply(a, -1)
+        if b < 0 :
+            negB = True
+            b = self.multiply(b, -1)
+        if b == 0 : return "Err"
+        elif a == 0 :return 0
+        else:
+            while a >= b :
+                a = self.subtract(a, b)
+                result = self.add(result, 1)
+
+        if negA :
+            result = self.multiply(result, -1)
+
+        if negB :
+            result = self.multiply(result, -1)
         return result
     
     def modulo(self, a, b):
-        while a <= b:
-            a = a-b
-        return a
+        if a == 0 : return "Err"
+        elif a < 0:
+            while a < 0:
+                a = self.add(a, b)
+        else:
+            while a >= b:
+                a = self.subtract(a, b)
+        return a 
 
 # Example usage:
 if __name__ == "__main__":
@@ -32,3 +58,4 @@ if __name__ == "__main__":
     print("Example: multiplication: ", calc.multiply(2, 3))
     print("Example: division: ", calc.divide(10, 2))
     print("Example: modulo: ", calc.modulo(10, 3))
+
